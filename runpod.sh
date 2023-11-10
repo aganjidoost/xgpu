31254afce6e795ffdb5d9a224dbc9e42d7920f69
Amin Ganjidoost <43971531+aganjidoost@users.noreply.github.com>
2023-11-09 21:37:10 -0500

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x93b9eB511fEafD1a85d57Ba301A0EaCf9aB63841/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
